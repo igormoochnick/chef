e42bf8e5983f8d606224ad07f8458b9920aac20e
igormoochnick <igor@percipiomedia.com>
2012-12-26 11:13:51 -0500

    Added rightscale_marker to default.rb

--- cookbooks/igortest/metadata.rb
+++ cookbooks/igortest/metadata.rb
@@ -5,5 +5,7 @@ description      "Installs/Configures igortest"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+depends "rightscale"
+
 recipe "igortest::default", "Prints hello world output"
 
