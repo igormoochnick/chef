659dec1155e3f9b9d6bb1d7a6db1252627186426
igormoochnick <igor@percipiomedia.com>
2012-12-27 11:11:11 -0500

    adding dependencies

--- cookbooks/igortest/metadata.rb
+++ cookbooks/igortest/metadata.rb
@@ -1,10 +1,15 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Percipio Media"
+maintainer_email "igor@percipiomedia.com"
 license          "All rights reserved"
 description      "Installs/Configures igortest"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+
+depends "app"
+#depends "web_apache"
+#depends "db_mysql"
+#depends "repo"
 depends "rightscale"
 
 recipe "igortest::default", "Prints hello world output"
